bdd61788b78aac3a92a4c4bf598eed53a609d32d
Tony-Tor <Tony.Tor2016@yandex.ru>
2023-10-01 16:39:19 +0400

    connect Postgresql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS purchase (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id serial primary key,
     product varchar(50) NOT NULL,
-    price double NOT NULL
+    price float8 NOT NULL
 );
